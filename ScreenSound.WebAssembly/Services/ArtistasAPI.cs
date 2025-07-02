3d0c6444c9f150f26a29fe8b2cedba0c086a4acf
marcelooliveira <mclricardo@gmail.com>
2025-07-01 22:31:41 -0300

    4786-video1.6-Criando ArtistasAPI

---
+++ ScreenSound.WebAssembly/Services/ArtistasAPI.cs
@@ -0,0 +1,20 @@
+﻿using ScreenSound.Shared.Modelos.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.WebAssembly.Services
+{
+    public class ArtistasAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public ArtistasAPI(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("API");
+        }
+
+        public async Task<ICollection<ArtistaResponse>> GetArtistasAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+        }
+    }
+}
